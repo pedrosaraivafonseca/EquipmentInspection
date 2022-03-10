89cf2e119b1d4549703533a034bbdad0a5d42e05
Pedro <pedro.saraivaf@hes-so.ch>
2022-03-10 15:05:44 +0100

    DAO - Entity

--- app/src/main/java/com/example/equipmentinspection/ui/MainActivity.java
+++ app/src/main/java/com/example/equipmentinspection/ui/MainActivity.java
@@ -8,6 +8,7 @@ import com.example.equipmentinspection.R;
 
 public class MainActivity extends AppCompatActivity {
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
