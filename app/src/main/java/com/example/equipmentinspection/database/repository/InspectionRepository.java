79ac1891b3b306a16bf49e41eb1cab863b9f1561
Pedro <pedro.saraivaf@hes-so.ch>
2022-03-10 17:28:37 +0100

    DB

--- app/src/main/java/com/example/equipmentinspection/database/repository/InspectionRepository.java
+++ app/src/main/java/com/example/equipmentinspection/database/repository/InspectionRepository.java
@@ -1,5 +1,18 @@
 package com.example.equipmentinspection.database.repository;
 
+import android.content.Context;
+
+import androidx.lifecycle.LiveData;
+
+import com.example.equipmentinspection.database.AppDatabase;
+import com.example.equipmentinspection.database.async.InspectionCreate;
+import com.example.equipmentinspection.database.async.InspectionDelete;
+import com.example.equipmentinspection.database.async.InspectionUpdate;
+import com.example.equipmentinspection.database.entity.InspectionEntity;
+import com.example.equipmentinspection.util.OnAsyncEventListener;
+
+import java.util.List;
+
 public class InspectionRepository {
 
     private static InspectionRepository instance;
@@ -17,35 +30,27 @@ public class InspectionRepository {
         return instance;
     }
 
-//    public LiveData<InspectionEntity> getInspection(final int id, Context context) {
-//        return AppDatabase.getInstance(context).InspectionDao().getById(id);
-//    }
-//
-//    public LiveData<InspectionEntity> getInspection(final int idInspector, Context context) {
-//        return AppDatabase.getInstance(context).InspectionDao().getByInspector(id);
-//    }
-//
-//    public LiveData<InspectionEntity> getInspection(final int idInspection, Context context) {
-//        return AppDatabase.getInstance(context).InspectionDao().getByInspector(id);
-//    }
-//
-//    public LiveData<InspectionEntity> getInspection(final String status, Context context) {
-//        return AppDatabase.getInstance(context).InspectionDao().getByStatus(status);
-//    }
-//
-//    public LiveData<List<InspectionEntity>> getAllInspection(Context context) {
-//        return AppDatabase.getInstance(context).InspectionDao().getAll();
-//    }
-//
-//    public void insert(final InspectionEntity inspection, OnAsyncEventListener callback, Context context) {
-//        new InspectionCreate(context, callback).execute(inspection);
-//    }
-//
-//    public void update(final InspectionEntity inspection, OnAsyncEventListener callback, Context context) {
-//        new InspectionUpdate(context, callback).execute(inspection);
-//    }
-//
-//    public void delete(final InspectionEntity inspection, OnAsyncEventListener callback, Context context) {
-//        new InspectionDelete(context, callback).execute(inspection);
-//    }
+    public LiveData<InspectionEntity> getInspection(final int id, Context context) {
+        return AppDatabase.getInstance(context).inspectionDao().getById(id);
+    }
+
+    public LiveData<InspectionEntity> getInspection(final String status, Context context) {
+        return AppDatabase.getInstance(context).inspectionDao().getByStatus(status);
+    }
+
+    public LiveData<List<InspectionEntity>> getAllInspection(Context context) {
+        return AppDatabase.getInstance(context).inspectionDao().getAll();
+    }
+
+    public void insert(final InspectionEntity inspection, OnAsyncEventListener callback, Context context) {
+        new InspectionCreate(context, callback).execute(inspection);
+    }
+
+    public void update(final InspectionEntity inspection, OnAsyncEventListener callback, Context context) {
+        new InspectionUpdate(context, callback).execute(inspection);
+    }
+
+    public void delete(final InspectionEntity inspection, OnAsyncEventListener callback, Context context) {
+        new InspectionDelete(context, callback).execute(inspection);
+    }
 }
--- app/src/main/java/com/example/equipmentinspection/database/repository/InspectorRepository.java
+++ app/src/main/java/com/example/equipmentinspection/database/repository/InspectorRepository.java
@@ -1,5 +1,18 @@
 package com.example.equipmentinspection.database.repository;
 
+import android.content.Context;
+
+import androidx.lifecycle.LiveData;
+
+import com.example.equipmentinspection.database.AppDatabase;
+import com.example.equipmentinspection.database.async.InspectorCreate;
+import com.example.equipmentinspection.database.async.InspectorDelete;
+import com.example.equipmentinspection.database.async.InspectorUpdate;
+import com.example.equipmentinspection.database.entity.InspectorEntity;
+import com.example.equipmentinspection.util.OnAsyncEventListener;
+
+import java.util.List;
+
 public class InspectorRepository {
 
     private static InspectorRepository instance;
@@ -18,27 +31,27 @@ public class InspectorRepository {
     }
 
     //
-//    public LiveData<InspectorEntity> getInspector(final int id, Context context) {
-//        return AppDatabase.getInstance(context).InspectorDao().getById(id);
-//    }
-//
-//    public LiveData<InspectorEntity> getInspector(final String email, final String password, Context context) {
-//        return AppDatabase.getInstance(context).InspectorDao().getByLogin(email, password);
-//    }
-//
-//    public LiveData<List<InspectorEntity>> getAllInspector(Context context) {
-//        return AppDatabase.getInstance(context).InspectorDao().getAll();
-//    }
-//
-//    public void insert(final InspectorEntity inspector, OnAsyncEventListener callback, Context context) {
-//        new InspectorCreate(context, callback).execute(inspector);
-//    }
-//
-//    public void update(final InspectorEntity inspector, OnAsyncEventListener callback, Context context) {
-//        new InspectorUpdate(context, callback).execute(inspector);
-//    }
-//
-//    public void delete(final InspectorEntity inspector, OnAsyncEventListener callback, Context context) {
-//        new InspectorDelete(context, callback).execute(inspector);
-//    }
+    public LiveData<InspectorEntity> getInspector(final int id, Context context) {
+        return AppDatabase.getInstance(context).inspectorDao().getById(id);
+    }
+
+    public LiveData<InspectorEntity> getInspector(final String email, final String password, Context context) {
+        return AppDatabase.getInstance(context).inspectorDao().getByLogin(email, password);
+    }
+
+    public LiveData<List<InspectorEntity>> getAllInspector(Context context) {
+        return AppDatabase.getInstance(context).inspectorDao().getAll();
+    }
+
+    public void insert(final InspectorEntity inspector, OnAsyncEventListener callback, Context context) {
+        new InspectorCreate(context, callback).execute(inspector);
+    }
+
+    public void update(final InspectorEntity inspector, OnAsyncEventListener callback, Context context) {
+        new InspectorUpdate(context, callback).execute(inspector);
+    }
+
+    public void delete(final InspectorEntity inspector, OnAsyncEventListener callback, Context context) {
+        new InspectorDelete(context, callback).execute(inspector);
+    }
 }
